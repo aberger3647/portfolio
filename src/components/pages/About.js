4b64480830eb1b666ba652d85086ca6e73ecd6d5
aberger3647 <112574259+aberger3647@users.noreply.github.com>
2023-02-17 23:06:51 -0600

    center headshot

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -6,7 +6,7 @@ export default function About() {
         <h1>Alex Berger</h1>
         <h2>is a full-stack developer</h2>
         <p className="abouttext">with a design background. Currently pursuing an intensive coding certificate at The University of Texas at Austin. Strengths in communication, research, and picking up new skills.</p>
-        <div className="headshot">
+        <div className="headshotdiv">
             <img src='./img/headshot.jpg' className='headshot' alt="Black and white headshot of Alex" />
         </div>
     </div>
