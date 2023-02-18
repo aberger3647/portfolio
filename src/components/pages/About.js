382a81069589def9068c363fc1af28defa4d9523
aberger3647 <112574259+aberger3647@users.noreply.github.com>
2023-02-17 22:17:25 -0600

    style resume button

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -5,7 +5,7 @@ export default function About() {
     <div className="about">
         <h1>Alex Berger</h1>
         <h2>is a full-stack developer</h2>
-        <p id="about">with a design background. Currently pursuing an intensive coding certificate at The University of Texas at Austin. Strengths in communication, research, and picking up new skills.</p>
+        <p className="abouttext">with a design background. Currently pursuing an intensive coding certificate at The University of Texas at Austin. Strengths in communication, research, and picking up new skills.</p>
         <div className="headshot">
             <img src='./img/headshot.jpg' className='headshot' alt="Black and white headshot of Alex" />
         </div>
--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -6,9 +6,12 @@ const Resume = () => {
 
     return (
     <div className="resume">
-        <h1>Resume</h1>
+        <h1>Proficiencies</h1>
         <div className="skills">
             {skills.map((skill) => <Skill skill={skill} />)}
+        </div>
+        <div className='resumelinkdiv'>
+            <a href='./img/AlexBergerResume2023.pdf' className='resumelink' target='_blank'><p className='resumetext'>Download Resume</p></a>
         </div>
     </div>
 )};
