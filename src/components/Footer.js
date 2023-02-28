635a24e31afce5de5a114e6d4fd9052c04293da7
aberger3647 <112574259+aberger3647@users.noreply.github.com>
2023-02-28 12:25:31 -0600

    position header elements

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => (
     <footer>
-        <div className="footer">
+        <div className="footerlinks">
             <a href="https://github.com/aberger3647" target="_blank" rel="noopener noreferrer" className="foot-link">GitHub</a>
             <a href="https://www.linkedin.com/in/alexjberger/" target="_blank" rel="noopener noreferrer" className="foot-link">LinkedIn</a>
             <a href="https://alexjberger.com" target="_blank" rel="noopener noreferrer" className="foot-link">Design</a>
